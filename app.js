b9f509783101c49599a623291cb081c305cb7918
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-05-24 06:50:47 +0900

    Saving Username

--- app.js
+++ app.js
@@ -9,16 +9,10 @@ function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem("username", username);
   console.log("username >", username);
-  // greeting.innerText = "Hello " + username;
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
-}
-
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
