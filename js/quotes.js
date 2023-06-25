f4217c344b4b0c5cd7201d74750575ed8432757f
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-06-25 15:19:27 +0900

    Setup

--- js/quotes.js
+++ js/quotes.js
@@ -49,8 +49,6 @@ const quotes = [
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
-console.log("quote", quotes[Math.floor(Math.random() * quotes.length)]);
-
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerHTML = todaysQuote.quote;
