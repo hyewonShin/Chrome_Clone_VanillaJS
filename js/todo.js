34690b9d15ec0a84e36ba96076b1b1862419e936
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-06-26 22:39:00 +0900

    Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+  console.log("I will paint", newTodo);
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+  console.log(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   console.log(toDoInput.value);
   toDoInput.value = "";
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
